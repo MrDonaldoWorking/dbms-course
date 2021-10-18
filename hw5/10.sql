238e8540e672d7eb97a05f089e71c1df2edca613
MrDonaldoPublic <mr.donaldo.cf@gmail.com>
2021-10-18 13:07:21 +0300

    Others solutions

---
+++ hw5/10.sql
@@ -0,0 +1,8 @@
+select distinct StudentId,
+                count(distinct CourseId)               as Total,
+                count(Mark)                            as Passed,
+                count(distinct CourseId) - count(Mark) as Failed
+from Students
+         left join (select GroupId, CourseId from Plan) query using (GroupId)
+         left join Marks using (StudentId, CourseId)
+group by StudentId
